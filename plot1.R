c689c0bc93443c8d090f44042497f572668c99f1
overizoid <68402523+overizoid@users.noreply.github.com>
2020-08-06 09:46:40 -0400

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,7 @@
+#Before creating the plots, I read the provided data and attached it to the variable "data".
+
+#Creating the histogram
+hist(as.numeric(as.character(data$Global_active_power)), main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
+
+#Export png
+dev.copy(png, "plot1.png")
