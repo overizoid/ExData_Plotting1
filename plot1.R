837b34e57284d013a1d7f9e0d1a4027ce899fba4
overizoid <68402523+overizoid@users.noreply.github.com>
2020-08-06 09:56:32 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,4 +1,5 @@
 #Before creating the plots, I read the provided data and attached it to the variable "data".
+data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, stringsAsFactors = FALSE)
 
 #Creating the histogram
 hist(as.numeric(as.character(data$Global_active_power)), main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red")
