e6e0ef25d3b76506efd719c2b96c2b2ee3c75e80
overizoid <68402523+overizoid@users.noreply.github.com>
2020-08-06 09:49:10 -0400

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,9 @@
+#Create Date/Time column
+
+data$Date_Time <- strptime(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S")
+
+#Create plot
+plot(data$Date_Time, data$Global_active_power, xlab = " ", ylab = "Gloabl Active Power (kilowatts)", type = "l")
+
+#Export png
+dev.copy(png, "plot2.png")
